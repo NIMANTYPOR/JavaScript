a575c2c2a4f8d6e4e9459b300b749558cab5f144
NIMANTYPOR <samirhasanov@yahoo.com>
2023-02-24 14:14:22 +0400

    message

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,25 @@
+// console.log("jonas");
+
+// let Firstname = "BOB";
+// console.log(Firstname);
+
+// let Javascript = true;
+// console.log(Javascript);
+// console.log(typeof Javascript);
+
+// const ageSamir = 2023 - 1994;
+// const ageXalil = 2023 - 1990;
+// console.log(ageSamir, ageXalil);
+
+// let MarkBMI = 100 / 1.69 ** 2;
+// let JohnBMI = 92 / 1.95 ** 2;
+// let markHigherBMI = MarkBMI > JohnBMI;
+// console.log(MarkBMI, JohnBMI, markHigherBMI);
+
+const Firstname = "Samir";
+const year = 2022;
+const birthyear = 1994;
+const job = "programmer";
+
+const samirmuellim = `I'm ${Firstname}, a ${year - birthyear} years ${job} `;
+console.log(samirmuellim);
